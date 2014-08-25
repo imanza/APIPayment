d68c644955718890b9292f289a89db3742db7742
imanza <iman_z721@yahoo.com>
2014-08-25 22:32:47 +0430

    Added HttpStatusCode in response camelCase output in json format (WebapiConfig) minor change in modelfactory

--- APIRestPayment/App_Start/WebApiConfig.cs
+++ APIRestPayment/App_Start/WebApiConfig.cs
@@ -11,7 +11,7 @@ namespace APIRestPayment
     {
         public static void Register(HttpConfiguration config)
         {
-
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             config.Routes.MapHttpRoute(
             name: "Payments",
